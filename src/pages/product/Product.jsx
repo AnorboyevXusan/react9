29226792d5f6ccd573b3892e0ca462812bb11ded
AnorboyevXusan <xusananor9@gmail.com>
2023-10-11 15:18:06 +0500

    commitda

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,18 +1,18 @@
-import {useEffect, useState} from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import './Product.scss'
-import {useParams} from "react-router";
-import axios from "axios";
 
 const Product = () => {
-    const { id } = useParams()
+    const { id } = useParams();
 
-    const [search, setSearch] = useState('')
-    const [product, setProduct] = useState([])
-    const [currentPage, setCurrentPage] = useState(1)
-    const [itemsPerPage] = useState(5)
+    const [search, setSearch] = useState('');
+    const [product, setProduct] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(10);
 
     useEffect(() => {
-        axios.get(`https://65263b7d67cfb1e59ce8075c.mockapi.io/api/category/${id || 1}/product`)
+        axios.get(`https://65263b7d67cfb1e59ce8075c.mockapi.io/api/category/${id}/product`)
             .then(res => {
                 setProduct(res.data);
             });
@@ -22,11 +22,32 @@ const Product = () => {
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentItems = product
-        .filter(i => i.name.toLowerCase().includes(search.toLowerCase()))
-        .slice(indexOfFirstItem, indexOfLastItem);
+        .filter(i => i.name.toLowerCase().includes(search.toLowerCase()));
 
-    // Change page
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const renderItems = currentItems.map(i => (
+        <li className='card-st' key={i.id}>
+            <img src={i.img} alt="img" />
+            <span>{i.name}</span>
+            <p>{i.description}</p>
+            <span>{i.price}</span>
+        </li>
+    ));
+
+    const renderPagination = () => {
+        const pageNumbers = Math.ceil(currentItems.length / itemsPerPage);
+
+        return (
+            <ul className="pagination">
+                {Array.from({ length: pageNumbers }, (_, index) => index + 1).map(number => (
+                    <li key={number} onClick={() => paginate(number)}>
+                        {number}
+                    </li>
+                ))}
+            </ul>
+        );
+    };
 
     return (
         <div>
@@ -34,28 +55,12 @@ const Product = () => {
                 <input type="search" placeholder="Search . . ." onChange={(e) => setSearch(e.target.value)} />
             </div>
             <div>
-                <ul>
-                    {currentItems.map(i => (
-                        <li key={i.id}>
-                            <img src={i.img} alt="img" />
-                            <span>{i.name}</span>
-                            <p>{i.description}</p>
-                            <span>{i.price}</span>
-                        </li>
-                    ))}
+                <ul className='card'>
+                    {renderItems.slice(indexOfFirstItem, indexOfLastItem)}
                 </ul>
             </div>
             <div>
-                {/* Pagination */}
-                <ul className="pagination">
-                    {product
-                        .filter(i => i.name.toLowerCase().includes(search.toLowerCase()))
-                        .map((_, index) => (
-                            <li key={index} onClick={() => paginate(index + 1)}>
-                                {index + 1}
-                            </li>
-                        ))}
-                </ul>
+                {renderPagination()}
             </div>
         </div>
     );
